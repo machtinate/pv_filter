34f68ac6f9f91c9d1d03381a49a59f05bd5c4205
Jesse Macht <jesse.p.macht@gmail.com>
2023-04-01 00:15:43 -0400

    First working simulation with filter.

--- simulation.py
+++ simulation.py
@@ -10,16 +10,16 @@ import matplotlib.pyplot as plt
 
 from model import process_variable, fo_filter
 
-dt = 0.1
-m = process_variable(0.2, 0.7, 0., 0., dt, 0.05)
-f = fo_filter(30, 0, dt)
+dt = 0.01
+m = process_variable(0.2, 0.7, np.array([[0.], [0.]]), 0., dt, 0.1)
+f = fo_filter(10, 0, dt)
 
 t = np.arange(0, 30, dt)
 op = np.where(t < 1, 0, 1)
 
-pv = np.array([m.step(u) for u in op])
+pv = np.array([m.step(u) for u in op], dtype=object)
+pv_filt = np.array([f.filt(v) for v in pv], dtype=object)
 
-pv_filt = np.array([f.filt(v) for v in pv])
-
-plt.plot(t, pv)
-plt.plot(t, pv_filt)
+plt.plot(t, pv, label='Noise')
+plt.plot(t, pv_filt, label='Filter')
+plt.legend()
