7ae29aea775b8ae46c7d222aaa35fc57630573d2
Jesse Macht <jesse.p.macht@gmail.com>
2023-04-04 22:37:47 -0400

    Full working simulation with notebook.

--- model.py
+++ model.py
@@ -22,7 +22,6 @@ Assumptions:
     sampled in a discrete fashion. As such, the filter is designed as a
     discrete filter to emulate the characteristics of it's continuous
     counterpart.
-    - It is assumed that the 
     - It is assumed that the data to be filtered has been collected and stored
     prior to filtering. That is, the filter is not being applied in realtime on
     the PV, but instead as a batch operation on a set collected data.
@@ -32,13 +31,55 @@ Assumptions:
 import numpy as np
 
 class process_variable(object):
+    """
+    A simulated process variable (PV).
     
+    Defined as a linear time invariant (LTI) system with second order dynamics.
+    Zeta and wn (natural frequency) can be adjusted to simulate different
+    dynamics.
+    
+    After initial conditions are configured on instantiation, control inputs 
+    can be passed through the step() function to move the process around. Noise
+    is added to the output of the system, configurable with the sigma variable.
+    """
     def __init__(self, zeta, wn, x_0, u_0, dt, sigma):
+        """
+        Parameters
+        ----------
+        zeta : float
+            Damping factor.
+                - overdamped -> zeta > 1
+                - critically damped -> zeta = 1
+                - under damped -> 0 < zeta < 1
+        wn : float
+            Natural frequency. The larger the number, the faster the system
+            oscillates in time.
+        x_0 : np.array([float])
+            Initial state. 2x1 numpy vector. First entry is zeroeth order state
+            variable (e.g. displacement), second entry is the first order
+            state variable (e.g. velocity).
+        u_0 : float
+            Control input. No gain configurable gain in this system so the 
+            response is 1-to-1.
+        dt : float
+            Time constant. Time lapse between discrete samples of the system.
+        sigma : float
+            Noise standard deviation. Std dev of measurement noise.
+
+        Returns
+        -------
+        None.
+
+        """
         # initial conditions
         self.x = x_0
         self.u = u_0
+        
+        # dynamic and noise variables
         self.wn = wn
         self.sigma = sigma
+        
+        # initial output
         self.y = np.dot(np.array([wn**2, 0.]),  x_0)
         
         # discretized process model
@@ -46,23 +87,87 @@ class process_variable(object):
         self.Bd = np.array([[0.], [1.]]) * dt
         
     def step(self, u):
+        """        
+        Parameters
+        ----------
+        u : float
+            Control input. 1-to-1 gain, applied to the second order state
+            rate of change (e.g. acceleration).
+
+        Returns
+        -------
+        y : float
+            Process output. In this case, defined as the first order state
+            variable (e.g. displacement). Gaussian noise is applied with
+            standard deviation defined by sigma variable.
+
+        """
+        # apply the state update
         self.x = np.dot(self.Ad, self.x) + (self.Bd * u)
+        
+        # extract the new output
         self.y = np.dot(np.array([self.wn**2, 0.]),  self.x) + np.random.normal(0, self.sigma)
+        
         return self.y
 
         
 class fo_filter(object):
+    """
+    A first order low pass filter.
+    
+    The cutoff frequency is configurable at instantiation.
+    
+    For a discrete system, a low pass filter is equivalently modelled by an
+    exponentially weighted moving average algorithm. The smoothing factor is 
+    alpha, calculated by the relationship in the init function.
+    """
     def __init__(self, wc, z_0, dt):
+        """
+        Parameters
+        ----------
+        wc : float
+            Cutoff frequency. Defines the frequency above which the signal is 
+            attenuated.
+        z_0 : float
+            Initial filter state. Initial state of the variable to be filtered.
+        dt : float
+            Filter time constant. The time lapse between discrete samples of 
+            the variable to be filtered. Should be the same as the model.
+
+        Returns
+        -------
+        None.
+
+        """
+        # initialize filter state
         self.z = z_0
+        
+        # calculate the smoothing factor for the corresponding cutoff frequency
         self.alpha = dt / ((1/wc) + dt)
         
         # step counter
         self.i = 0
         
     def filt(self, data):
+        """
+        Parameters
+        ----------
+        data : float
+            New data point for the filter. Assumed to be the next piece of
+            information passed to the filtering algorithm in the time sequence.
+
+        Returns
+        -------
+        z : float
+            Filtered state. The filtered state after being passed through the
+            low-pass algorithm.
+
+        """
         if self.i == 0:
+            # skip the first time step, corresponds with the intitial condition
             self.i += 1
         else:
+            # apply the exponentially weighted smoothing algorithm
             self.z = self.alpha * data + (1-self.alpha) * self.z
             self.i += 1
         return self.z
--- simulation.py
+++ simulation.py
@@ -3,6 +3,9 @@
 Created on Fri Mar 31 22:51:57 2023
 
 @author: jmach
+
+A short script to demonstrate configuration of the PV and filter objects and a
+simulation over a defined time range in response to a control input step change.
 """
 
 import numpy as np
@@ -10,16 +13,28 @@ import matplotlib.pyplot as plt
 
 from model import process_variable, fo_filter
 
-dt = 0.01
-m = process_variable(0.2, 0.7, np.array([[0.], [0.]]), 0., dt, 0.1)
-f = fo_filter(10, 0, dt)
+# configure the sampling time constant
+dt = 0.05
 
+# configure the model and filter objects, note that the cutoff frequency for 
+# the filter should be chosen betwen the implied noise frequency that is 
+# dependent on the sample time constant e.g. w_noise = 1/dt
+m = process_variable(0.3, 0.5, np.array([[0.], [0.]]), 0., dt, 0.1)
+f = fo_filter(1, 0, dt)
+
+# create a time range and simulate a step change in control input (op) @ 5 sec
 t = np.arange(0, 30, dt)
-op = np.where(t < 1, 0, 1)
+op = np.where(t < 5, 0, 1)
 
+# first generate the noisy pv from the model, then apply the filter over it
 pv = np.array([m.step(u) for u in op], dtype=object)
 pv_filt = np.array([f.filt(v) for v in pv], dtype=object)
 
-plt.plot(t, pv, label='Noise')
-plt.plot(t, pv_filt, label='Filter')
+# plot the raw signal and the filtered signal over time
+plt.plot(t, op, label='Control input')
+plt.plot(t, pv, label='Noisy signal')
+plt.plot(t, pv_filt, label='Filtered signal')
+plt.title('Process Variable Filter Simulation')
+plt.xlabel('Time [s]')
+plt.ylabel('PV')
 plt.legend()
